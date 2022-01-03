9d40ffa8d87811548e7f09a633868187c96df9a6
Ikechukwu Israel <iykeprince48@gmail.com>
2022-01-03 16:33:25 +0100

    added js files & index.html

---
+++ js/hello.js
@@ -0,0 +1,8 @@
+document.getElementById("demo").innerHTML = "Hello Javascript";
+document.getElementById('demo').style.fontSize = '35px';
+document.getElementById('demo').style.color = 'green';
+        // document.getElementById('demo').style.display = 'none'
+
+document.write('my first javascript');
+console.log('i am doing well');
+window.alert("we're popping");
