62d73c39f8833de287f24056b02e9349fd85513b
Ikechukwu Israel <iykeprince48@gmail.com>
2022-01-04 16:59:20 +0100

    i am learning javascript

---
+++ js/comment.js
@@ -0,0 +1,10 @@
+
+//They are two types of comments in javascript 
+//1. Single line comment
+//2. Multi line comment
+ //Example of a single line comment - document.getElementById('demo').style.display = 'none'
+
+/** Example of a Multi-line comment:
+ *  i am going to be badass 
+ *  javascript programing
+ */
---
+++ js/variables.js
@@ -0,0 +1,67 @@
+var x = 5;
+var y = 9;
+var result = x + y;
+console.log(result)
+
+//let try working with 'let'
+ //String
+//Integer => 12
+//Double ~ float => 0.3249
+//Boolean => true/false
+// String => "jame123@gmail.com"
+// null|undefined
+// Object {}
+
+let email1 = "joan123@gmail.com";
+let email2 = "ola254@gmail.com";
+
+document.getElementById("user1").innerHTML = email1;
+document.getElementById("user2").innerHTML = email2;
+
+//
+let value1 = 2+9+8; 
+console.log (value1)
+
+let value2= "stanley"+" "+ "ikenna";
+console.log (value2)
+
+let value3 = "5" + 2 + 4;
+console.log("Arithmetic operation:", value3)
+
+//VALID VARIABLES
+let $value = "email@email.com";
+let _value = "@twitterhandle.co";
+let what_are_you_doing = '1value';
+
+let $value3 = 3**2;
+
+console.log('$value', $value)
+console.log("_value", _value);
+console.log("Exponent", $value3)
+
+
+// = Assignment Operator
+// + Addition operator
+// * Multiplication operator
+// 
+
+
+//Boolean operations
+let b1 = 4;
+let b2 = 5;
+
+console.log("is b1 equal to b2 ?", b1 == b2)
+console.log('is b1 equal to b1', b1 == b1)
+
+//Arrays
+const array1 = ["Benz", "Toyota", "Rolce Royce", "XCalade"];
+
+console.log('Array 1', array1)
+console.log('Size of the array', array1.length);
+console.log('first item in the array', array1[0])
+console.log('second item in the array', array1[1]);
+console.log('third item in the array', array1[2]);
+console.log('fourth item in the array', array1[3])
+console.log('this should undefined', array1[4])
+
+//Assignment to be submitted tomorrow morning
